69752e17bb3781bb5b44115d97cfa6487983afab
Miguel Bomfim <60234561+miguel-bomfim@users.noreply.github.com>
2024-12-16 11:53:08 -0300

    Update HomeGridPortfolio.tsx

--- src/components/HomeGridPortfolio.tsx
+++ src/components/HomeGridPortfolio.tsx
@@ -10,51 +10,6 @@ interface PortfolioGrid {
   ];
 }
 
-const portfolioItems = [
-  {
-    id: 1,
-    src: "https://images.unsplash.com/photo-1731323036230-fb37b4d9ed71?q=80&w=2069&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    alt: "Portfolio item 1",
-    width: 1200,
-    height: 800,
-  },
-  {
-    id: 2,
-    src: "https://plus.unsplash.com/premium_photo-1731329153355-1015daf2cb92?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    alt: "Portfolio item 2",
-    width: 800,
-    height: 1200,
-  },
-  {
-    id: 3,
-    src: "https://images.unsplash.com/photo-1734000402740-dc480cbbaeb6?q=80&w=1972&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    alt: "Portfolio item 3",
-    width: 1600,
-    height: 900,
-  },
-  {
-    id: 4,
-    src: "https://images.unsplash.com/photo-1732525830182-b0e1a93f3679?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    alt: "Portfolio item 4",
-    width: 900,
-    height: 900,
-  },
-  {
-    id: 5,
-    src: "https://images.unsplash.com/photo-1732254721629-bf8275f694e6?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    alt: "Portfolio item 5",
-    width: 1200,
-    height: 1600,
-  },
-  {
-    id: 6,
-    src: "https://plus.unsplash.com/premium_photo-1733922312592-5424a3ffdff6?q=80&w=2069&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    alt: "Portfolio item 6",
-    width: 1600,
-    height: 1200,
-  },
-];
-
 export default function PortfolioGrid({ portfolio }: PortfolioGrid) {
   return (
     <div className="container mx-auto px-4 py-24">
