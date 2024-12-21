2fb9153bb9dccbdcb304c61ee8799f087cf1fb84
Miguel Augusto <60234561+miguel-bomfim@users.noreply.github.com>
2024-12-21 13:18:55 +0000

    add fade animation to grid portfolio

--- src/components/HomeGridPortfolio.tsx
+++ src/components/HomeGridPortfolio.tsx
@@ -17,16 +17,35 @@ interface PortfolioGrid {
 
 export default function PortfolioGrid({ portfolio, mobile }: PortfolioGrid) {
   const [portfolioImages, setPortfolioImages] = useState(portfolio);
+  const [isFading, setIsFading] = useState(false);
 
   useEffect(() => {
     if (!mobile) {
-      setTimeout(() => setPortfolioImages(shuffle(portfolioImages)), 7000);
+      const interval = setInterval(() => {
+        // Trigger fade-out
+        setIsFading(true);
+
+        setTimeout(() => {
+          setPortfolioImages((portfolioImages) => shuffle(portfolioImages));
+
+          // Trigger fade-in
+          setTimeout(() => setIsFading(false), 500); // Match CSS fade duration
+        }, 500);
+      }, 4000);
+
+      return () => clearInterval(interval);
     }
+
+    // if (!mobile) {
+    //   setTimeout(() => setPortfolioImages(shuffle(portfolioImages)), 7000);
+    // }
   }, [portfolioImages]);
 
   return (
     <div className="container mx-auto px-4">
-      <div className="columns-1 sm:columns-3 lg:columns-4 gap-4 space-y-4">
+      <div
+        className={`columns-1 sm:columns-3 lg:columns-4 gap-4 space-y-4 grid-container `}
+      >
         {portfolioImages.slice(0, 8).map((item, idx) => (
           <div
             key={idx}
@@ -37,6 +56,9 @@ export default function PortfolioGrid({ portfolio, mobile }: PortfolioGrid) {
               alt=""
               width={item.width}
               height={item.height}
+              className={`${!mobile && "fade-image"} ${
+                isFading ? "fade-out" : "fade-in"
+              }`}
             />
             <div className="absolute inset-0 bg-black bg-opacity-0 transition-opacity duration-300 ease-in-out" />
           </div>
