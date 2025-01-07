024e4127274ebf036bcefdab194fa934134d9c0f
Miguel Bomfim <60234561+miguel-bomfim@users.noreply.github.com>
2025-01-06 21:37:27 -0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -12,10 +12,6 @@ const nextConfig: NextConfig = {
         protocol: "https",
         hostname: "plus.unsplash.com",
       },
-      {
-        protocol: "https",
-        hostname: "images.unsplash.com",
-      },
     ],
   },
 };
